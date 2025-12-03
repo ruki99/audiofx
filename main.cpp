069b1376f3e62486f8af60a0983aa484b49a1f63
ruki99 <ruqayyah.a.ali@gmail.com>
2025-12-03 18:27:45 -0400

    updated main to work with bitcrusher effect

--- main.cpp
+++ main.cpp
@@ -1,61 +1,43 @@
 #include "Effect.h"
 #include "Volume.h"
-#include <cstdint>
+#include "Bitcrusher.h"
+#include "WavFile.h"
 #include <iostream>
 #include <string>
-
-const int HEADER_SIZE = 44;
 
 int main(int argc, char* argv[]);
 
 int main(int argc, char* argv[]){
 
-    if (argc != 4){
-        std::cout << "usage ./AudioFilter input.wav output.wav <volume factor>" << std::endl;
+    // args for vol
+    // if (argc != 4){
+    //     std::cout << "usage ./AudioFX input.wav output.wav <volume factor>" << std::endl;
+    //     return 1;
+    // }
+
+    // args for bitcrusher
+    if (argc != 5){
+        std::cout << "usage ./AudioFX input.wav output.wav <resolution factor> <sample rate factor>" << std::endl;
         return 1;
     }
 
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL){
-        printf("Could not open file.\n");
-        return 1;
-    }
+    WavFile wav(argv[1]);
+    std::vector<float>& buffer = wav.getAudio();
 
-    FILE *output = fopen(argv[2], "w");
-    if (output == NULL)
-    {
-        printf("Could not open file.\n");
-        return 1;
-    }
+    // testing main with volume
+    // float vol_factor = std::stof(argv[3]);
+    // Effect* volFX = new Volume(vol_factor);
+    // volFX->process(buffer);
+    // wav.save(argv[2], buffer);
+    // delete volFX;
 
-    float vol_factor = std::stof(argv[3]);
-
-    // Copy header from input file to output file
-    uint8_t header[HEADER_SIZE]; // array of bytes to store 44 byte header
-    fread(header, HEADER_SIZE, 1, input); // read header from input file
-    fwrite(header, HEADER_SIZE, 1, output); // write header to output file
-
-    // Read samples from input file and write updated data to output file
-    // How? Loop through input till eof
-    // for each 2 byte sample, read into memory, update vol and write to output
-
-    Effect* myFX = new Volume(vol_factor);
-    int16_t sample;
-
-    // Read single sample from input into buffer while there are samples left to read
-    while (fread(&sample, sizeof(int16_t), 1, input) != 0){
-        // Update volume of sample
-        sample = myFX->process(sample);
-
-        // Write updated sample to new file
-        fwrite(&sample, sizeof(int16_t), 1, output);
-    }
-
-    // Close files
-    fclose(input);
-    fclose(output);
-
-    delete myFX;
+    // testing main with bitcrusher
+    int res = std::stof(argv[3]);
+    int rate = std::stof(argv[4]);
+    Effect* BitcrusherFX = new Bitcrusher(res, rate);
+    BitcrusherFX->process(buffer);
+    wav.save(argv[2], buffer);
+    delete BitcrusherFX;
 
     return 0;
 }
